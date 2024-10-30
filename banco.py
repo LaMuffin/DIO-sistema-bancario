06cd30c4d214cf35ed2f69b609a09089b4962343
Leon CS <104866877+LaMuffin@users.noreply.github.com>
2024-10-30 08:25:05 -0300

    Add files via upload

---
+++ banco.py
@@ -0,0 +1,73 @@
+name = input("informe o seu nome: ")
+
+print(f"Ola {name}, selecione uma das opcoes: ")
+
+menu = """
+
+[1] Depositar
+[2] Sacar
+[3] Extrato
+[4] Sair
+
+=> """
+
+saldo = 0
+limite = 500
+extrato = ""
+numero_saques = 0
+LIMITE_SAQUES = 3
+
+while True:
+
+    opcao = input(menu)
+
+    if opcao == "1":
+        valor = float(input("Informe o valor do depósito: "))
+
+        if valor > 0:
+            saldo += valor
+            extrato += f"Depósito: R$ {valor:.2f}\n"
+
+        else:
+            print("Operação falhou! O valor informado é inválido.")
+
+    elif opcao == "2":
+        print("\nO seu limite de saque é: R$ 500.00 ")
+        print(f"Voce pode fazer um total de {LIMITE_SAQUES} saques ")
+        valor = float(input("\nInforme o valor do saque: "))
+
+        excedeu_saldo = valor > saldo
+
+        excedeu_limite = valor > limite
+
+        excedeu_saques = numero_saques >= LIMITE_SAQUES
+
+        if excedeu_saldo:
+            print("ERRO! Você não tem saldo suficiente.")
+
+        elif excedeu_limite:
+            print("ERRO! O valor do saque excede o seu limite limite.")
+
+        elif excedeu_saques:
+            print("ERRO! Número máximo de saques excedido.")
+
+        elif valor > 0:
+            saldo -= valor
+            extrato += f"Saque: R$ {valor:.2f}\n"
+            numero_saques += 1
+
+        else:
+            print("Operação falhou! O valor informado é inválido.")
+
+    elif opcao == "3":
+        print("\n================ EXTRATO ================")
+        print(f"\nCliente: {name}")
+        print("Não foram realizadas movimentações." if not extrato else extrato)
+        print(f"\nSaldo: R$ {saldo:.2f}")
+        print("==========================================")
+
+    elif opcao == "4":
+        break
+
+    else:
+        print("Operação inválida, por favor selecione uma das opcoes disponiveis.")
